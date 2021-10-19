323173f9914d4b4d6607f07f606d9082fe22ac71
dallascaro <dallas_caro@yahoo.com>
2021-10-19 11:09:25 -0500

    added two screens with button control

--- App.js
+++ App.js
@@ -1,15 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, TextInput, View, Image, SafeAreaView, SectionList, ScrollView, Button } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({navigation}){
+  return(
+    <View>
+      <Text>Welcome to the Home Screen</Text>
       <StatusBar style="auto" />
+      <View> 
+        <Button
+        title = "Go to search Screen"
+        onPress={() => navigation.navigate('Search')}>
+        </Button>
+      </View>
     </View>
   );
 }
+
+function SearchScreen({navigation}){
+  return(
+    <View>
+      <Text>Welcome to the Search Screen</Text>
+      <StatusBar style="auto" />
+      <View> 
+        <Button
+        title = "Go to home Screen"
+        onPress={() => navigation.navigate('Search')}>
+        </Button>
+      </View>
+    </View>
+
+  );
+
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Search" component={SearchScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +61,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
